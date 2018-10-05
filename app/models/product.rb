e75cb4f433e8f08baf229b3e95593052f6641d84
文圆承 <330469102@qq.com>
2018-10-05 13:37:33 +0800

    增加了产品标题验证 lenght 和 自定义 message.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
-  validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}  #确认价格是否有效
+  validates :title, :description, :image_url, presence: { message: "字段不能为空"}
+  validates :title, uniqueness: true, allow_blank: true, length: { minimum: 10, message: "不能少于10个字符"}
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }  #确认价格是否有效
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG, PNG'
