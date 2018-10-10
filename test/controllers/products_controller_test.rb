b08686d5248856e5a68689f4a9899f11d5a8d9c0
文圆承 <330469102@qq.com>
2018-10-10 16:32:15 +0800

    完成了任务G: 订单结算的部分

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,7 +15,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     @update = {
       title:       'Lorem Ipsum',
       description: 'Wibbles are fun!',
-      image_url:   'lorem.jpg',
+      image_url:   'adrpo.jpg',
       price:       19.95
     }
   end
